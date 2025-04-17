4db55857f0053d90bbdddf03d32c109caa536a4b
pedroevaristo <pedrodzprogramacao@gmail.com>
2025-04-17 13:47:00 -0300

    refazendo o algoritmo de shunting yard

--- src/Algorithm/ShuntingYardAlgorthm.java
+++ src/Algorithm/ShuntingYardAlgorthm.java
@@ -2,21 +2,53 @@ package Algorithm;
 
 import java.util.Stack;
 import java.util.Map;
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 import BasicOperationsMath.MathOperation;
 import BasicOperationsMath.MathUtils;
 
 public class ShuntingYardAlgorthm {// aqui vai ter o algoritmo que vai converter e verificar as expressão passad no
 									// input
-	private final Map<String, MathOperation> operationMap;
-	private final Map<Character, Integer> initializeLocalOperationMath;
-
+	// private final Map<String, MathOperation> operationMap;
+	// private final Map<Character, Integer> initializeLocalOperationMath;
 
 	public String shuntingYardAlgorithm(String expression) {// 2x+5=9, x^8,
+
+		Deque<String> operatorsStack = new ArrayDeque<>();
+		List<String> outputPostFix= new ArrayList<>();
 		
+		String[] tokens = expression.split("\\s+");
+		for (String token : tokens) {
+			if(token.matches("-?\\d+(\\.\\d+)?")) {
+							outputPostFix.add(token);	
+			}
+			else if(MathUtils.VALID_OPERATORS.contains(token.charAt(0))) {
+				while(!operatorsStack.isEmpty() &&
+						MathUtils.VALID_OPERATORS.contains(operatorsStack.peek().charAt(0)) &&
+								MathUtils.PRIORITY.get(Character.valueOf(operatorsStack.peek().charAt(0)))
+								>= MathUtils.PRIORITY.get(Character.valueOf(token.charAt(0)))) {
+					outputPostFix.add(operatorsStack.pop());
+				}
+				operatorsStack.push(token);
+			}else if(token.equals("(")) {
+				operatorsStack.push(token);				
+			}else if(token.equals(")")) {
+				while(!operatorsStack.isEmpty() && !operatorsStack.peek().equals("(")) {
+					outputPostFix.add(operatorsStack.pop());					
+				}
+				while(!operatorsStack.isEmpty()) {
+					outputPostFix.add(operatorsStack.pop());
+				}
+			}
 
-		StringBuilder output = new StringBuilder();
-		Stack<Character> stack = new Stack<>();
-
+		}
+		return ;
+	}
 
 }
--- src/BasicOperationsMath/MathUtils.java
+++ src/BasicOperationsMath/MathUtils.java
@@ -1,8 +1,26 @@
 package BasicOperationsMath;
 
+import java.util.Map;
 import java.util.Set;
 
 public class MathUtils {
-	public static final Set<Character> VALID_OPERATORS = Set.of('+', '-', '*', '/', '^');
-	public static final Set<Character> VALID_CHARACTERS = Set.of('(', ')','=');
+	public static final Set<Character> VALID_OPERATORS = Set.of(
+			'+',
+			'-',
+			'*',
+			'/',
+			'^'
+			);
+	public static final Set<Character> VALID_CHARACTERS = Set.of(
+			'(',
+			')',
+			'='
+			);
+	public static final Map<Character, Integer> PRIORITY = Map.of(
+			'+', 1,
+			'-', 1,
+			'*', 2,
+			'/', 2,
+			'^', 3
+	);
 }
